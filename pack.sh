ece63aeee25d6ac0ac3a9c1a7e2195b773970049
Ufuk Kayserilioglu <ufuk@enkuba.com>
2014-05-28 15:17:47 +0300

    Pack script should output to the dist folder

--- pack.sh
+++ pack.sh
@@ -1,6 +1,7 @@
-pushd "`dirname \"$0\"`/.."
-version=`grep "version =" PayUSharp/PayUSharp.sln | awk '{print $3}' | tr -d ' \r\n'`
-output=PayUSharp-v$version.7z
+pushd "`dirname \"$0\"`"
+mkdir -p "dist"
+version=`grep "version =" PayUSharp.sln | awk '{print $3}' | tr -d ' \r\n'`
+output=dist/PayUSharp-v$version.7z
 rm $output
-7za a -r -x!.git -x!*.mdb -x!pack.sh -x!*.txt -x!*.md -x!*.pdf -x!.DS_Store -x!*.userprefs -x!Debug -x!obj $output PayUSharp/*
+7za a -r -x!.git -x!*.mdb -x!pack.sh -x!*.txt -x!*.md -x!*.pdf -x!.DS_Store -x!*.userprefs -x!Debug -x!obj -x!dist $output ./*
 popd
